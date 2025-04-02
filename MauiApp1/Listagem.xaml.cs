1aa1aa3fc449ab1ae406519a3b9e3c13abab73b3
Conrado Saud <50385963+conradosaud@users.noreply.github.com>
2025-04-02 19:59:07 -0300

    Tela de cadastro

--- MauiApp1/Listagem.xaml.cs
+++ MauiApp1/Listagem.xaml.cs
@@ -5,52 +5,30 @@ namespace MauiApp1;
 public partial class Listagem : ContentPage
 {
 
-	public Listagem()
+    List<Pessoa> pessoas = new List<Pessoa>();
+
+    public Listagem()
 	{
 		InitializeComponent();
 
 		Pessoa p1 = new Pessoa();
 		p1.nome = "Conradito";
 		p1.idade = 15;
-		p1.id = 0;
 
 		Pessoa p2 = new Pessoa();
-		p2.idade = 20;
 		p2.nome = "Pedrita";
-		p2.id = 1;
+		p2.idade = 20;
 
 		Pessoa p3 = new Pessoa();
 		p3.nome = "Juanzito";
 		p3.idade = 19;
-		p3.id = 2;
 
-		Pessoa.pessoas.Add(p1);
-		Pessoa.pessoas.Add(p2);
-		Pessoa.pessoas.Add(p3);
+		pessoas.Add(p1);
+		pessoas.Add(p2);
+		pessoas.Add(p3);
 
-		Lista.ItemsSource = Pessoa.pessoas;
+		Lista.ItemsSource = pessoas;
 
 	}
 
-    protected override void OnAppearing()
-    {
-        base.OnAppearing();
-		Lista.ItemsSource = null;
-        Lista.ItemsSource = Pessoa.pessoas;
-    }
-
-    private async void Button_Clicked(object sender, EventArgs e)
-    {
-		Button button = (Button)sender;
-		int id = (int)button.CommandParameter;
-
-		bool confirma = await DisplayAlert("Excluir usuário", "Tem certeza que deseja excluir?", "Sim", "Não");
-		if(confirma == true)
-		{
-			Pessoa.pessoas.RemoveAt(id);
-			Lista.ItemsSource = null;
-			Lista.ItemsSource = Pessoa.pessoas;
-		}
-
-    }
 }
--- MauiApp1/Pessoa.cs
+++ MauiApp1/Pessoa.cs
@@ -9,9 +9,6 @@ namespace MauiApp1
     class Pessoa
     {
 
-        public static List<Pessoa> pessoas = new List<Pessoa>();
-
-        public int id { get; set; }
         public string nome { get; set; }
         public int idade { get; set; }
 
